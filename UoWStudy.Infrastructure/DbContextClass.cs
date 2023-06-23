83d2edc8e34d94e9b4303033ee01023d90f00ffe
Averkios Karalis <averkkaralis@gmail.com>
2023-06-23 16:35:52 +0300

    asdf

--- UoWStudy.Infrastructure/DbContextClass.cs
+++ UoWStudy.Infrastructure/DbContextClass.cs
@@ -10,7 +10,10 @@ namespace UoWStudy.Infrastructure {
     public class DbContextClass : DbContext {
         public DbContextClass(DbContextOptions<DbContextClass> options) : base(options)
         {
-            
+            //The constructor DbContextClass(DbContextOptions<DbContextClass> options) : base(options) is defined
+            //to initialize the DbContextClass instance.
+            //It calls the base class constructor passing the options parameter to set up the database connection
+            //and configuration.  
         }
         public DbSet<Product> Products { get; set; }
     }
--- UoWStudy.Infrastructure/ServiceExtension/ServiceExtension.cs
+++ UoWStudy.Infrastructure/ServiceExtension/ServiceExtension.cs
@@ -1,10 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using UoWStudy.Core.Interfaces;
+using UoWStudy.Infrastructure.Repositories;
 
-namespace UoWStudy.Infrastructure.ServiceExtension {
-    internal class ServiceExtension {
+namespace UoWStudy.Infrastructure.ServiceExtension;
+// in this class, we are registering the Dependency Injection services and configure that inside the Program.cs file inside
+// the root project
+    public static class ServiceExtension {
+        public static IServiceCollection DIServices (this IServiceCollection services, IConfiguration config) {
+            services.AddDbContext<DbContextClass>(options =>
+                options.UseSqlServer(config.GetConnectionString("DefaultConnection")));
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IProductRepository, IProductRepository>();
+
+            return services;
+        }
     }
-}
+
